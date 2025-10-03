bcbecee6c6722bf3e1446c7cfe844e9270fb1c3f
joseolinda <joseolindad@yahoo.com.br>
2025-10-03 19:35:51 -0300

    novo visual do select role

--- resources/views/select-role.blade.php
+++ resources/views/select-role.blade.php
@@ -1,38 +1,32 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+@extends('app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acessar como</title>
-    <link rel="stylesheet" href="{{ asset('css/select-role.css') }}">
-</head>
+@section('title', 'Escolher Perfil')
 
-<body>
-    <div class="container">
-        <h2>Acessar como:</h2>
-        <div class="options">
-            <a href="{{ route('login.aluno') }}" class="card">
-                <span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="40" height="40">
-                        <path
-                            d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z" />
-                    </svg>
-                </span>
-                <p>Aluno</p>
-            </a>
+@section('content')
+<div class="role-container">
+    <h2 class="title">Escolha como deseja acessar o BibWeb</h2>
+    <div class="role-options">
+        <a href="{{ route('login.aluno') }}" class="role-card student">
+            <div class="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="48" height="48">
+                    <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 
+                        2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 
+                        5v3h20v-3c0-3.3-6.7-5-10-5z"/>
+                </svg>
+            </div>
+            <p>Aluno</p>
+        </a>
 
-            <a href="{{ route('login.bibliotecario') }}" class="card">
-                <span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="40" height="40">
-                        <path
-                            d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 5v3h20v-3c0-3.3-6.7-5-10-5z" />
-                    </svg>
-                </span>
-                <p>Bibliotecário</p>
-            </a>
-        </div>
+        <a href="{{ route('login.bibliotecario') }}" class="role-card librarian">
+            <div class="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="48" height="48">
+                    <path d="M12 12c2.7 0 5-2.3 5-5s-2.3-5-5-5-5 
+                        2.3-5 5 2.3 5 5 5zm0 2c-3.3 0-10 1.7-10 
+                        5v3h20v-3c0-3.3-6.7-5-10-5z"/>
+                </svg>
+            </div>
+            <p>Bibliotecário</p>
+        </a>
     </div>
-</body>
-
-</html>
+</div>
+@endsection
